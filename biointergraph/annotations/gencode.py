78f40a51e3f06fd4bd2f37211e1e7077eb3e27fe
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2024-11-21 14:54:28 +0300

    Add kwargs parameter to load_gencode_annotation function

--- biointergraph/annotations/gencode.py
+++ biointergraph/annotations/gencode.py
@@ -37,7 +37,7 @@ def load_gencode_annotation(
         content: str = 'comprehensive',
         format: str = 'gff3',
         verbose: bool = False,
-        chunksize: int|None = None
+        **kwargs
     ) -> pd.DataFrame:
 
     ASSEMBLIES = ('hg19', 'hg38', 'GRCh37', 'GRCh38')
@@ -87,7 +87,7 @@ def load_gencode_annotation(
         f'.annotation.{format}.gz'
     ])
 
-    table = read_feature_table(full_path, chunksize=chunksize)
+    table = read_feature_table(full_path, **kwargs)
     if verbose:
         print(f'GENCODE annotation URL:\n\t{full_path}')
         print(f'Feature table shape:\n\t{table.shape}')
