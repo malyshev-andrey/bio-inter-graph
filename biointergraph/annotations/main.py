8c24594a89348cb7a1463c453a27feb9abb2df76
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2024-11-21 15:36:57 +0300

    Add data validation functionality to read_feature_table function

--- biointergraph/annotations/main.py
+++ biointergraph/annotations/main.py
@@ -1,3 +1,5 @@
+from warnings import warn
+
 import pandas as pd
 from tqdm.auto import tqdm
 
@@ -7,10 +9,38 @@ GFF_COLUMNS = [
     'strand', 'phase', 'attributes'
 ]
 
+
+def _validate_feature_table(ft: pd.DataFrame) -> pd.DataFrame:
+    is_valid_start = ft['start'].str.isdigit()
+    is_valid_end = ft['end'].str.isdigit()
+    is_valid_strand = ft['strand'].isin({'+', '-', '.'})
+    if not is_valid_start.all():
+        incorrect = " ".join(ft['start'][~is_valid_start].head(5))
+        warn(
+            f'Some start coordinates seem to be incorrect: {incorrect}.\n'
+            'Skipping these lines!'
+        )
+    if not is_valid_end.all():
+        incorrect = " ".join(ft['end'][~is_valid_end].head(5))
+        warn(
+            f'Some end coordinates seem to be incorrect: {incorrect}.\n'
+            'Skipping these lines!'
+        )
+    if not is_valid_strand.all():
+        incorrect = " ".join(ft['strand'][~is_valid_strand].head(5))
+        warn(
+            f'Some strand values seem to be incorrect: {incorrect}.\n'
+            'Skipping these lines!'
+        )
+    ft = ft[is_valid_strand & is_valid_end & is_valid_start]
+    return ft
+
+
 def read_feature_table(
         path: str, *,
         filter_func=lambda df: df,
-        chunksize: int|None = None
+        chunksize: int|None = None,
+        validation: bool = True
     ) -> pd.DataFrame:
 
     kwargs = dict(
@@ -20,7 +50,7 @@ def read_feature_table(
     )
 
     if chunksize is None:
-        return filter_func(pd.read_csv(path, **kwargs))
+        result = filter_func(pd.read_csv(path, **kwargs))
     else:
         result = []
         desc = path if len(path) < 40 else path[:20] + ' ... ' + path[-20:]
@@ -28,4 +58,9 @@ def read_feature_table(
             for chunk in pd.read_csv(path, chunksize=chunksize, **kwargs):
                 progress_bar.update(chunk.shape[0])
                 result.append(filter_func(chunk))
-        return pd.concat(result)
+        result = pd.concat(result)
+
+    if validation:
+        result = _validate_feature_table(result)
+
+    return result
