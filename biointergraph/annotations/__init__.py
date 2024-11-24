4e436bdf2ef65628e9b8476f5a31e7c516b914cf
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2024-11-24 19:45:08 +0300

    Add unify_chr function to __init__.py file in annotations module

--- biointergraph/annotations/__init__.py
+++ biointergraph/annotations/__init__.py
@@ -1,11 +1,12 @@
-from .main import read_feature_table
+from .main import read_feature_table, unify_chr
 from .gencode import load_gencode_annotation
 from .refseq import load_refseq_annotation
 from .gff2bed import gff2bed
 from .intersect import bed_intersect
 
+
 __all__ = [
-    'read_feature_table',
+    'read_feature_table', 'unify_chr',
     'load_gencode_annotation',
     'load_refseq_annotation',
     'gff2bed',
