691f4af671218e132cf3d4ca2c761a658d434e45
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2024-11-24 17:39:20 +0300

    Add bed_intersect function to __init__.py file of annotations module

--- biointergraph/annotations/__init__.py
+++ biointergraph/annotations/__init__.py
@@ -2,10 +2,12 @@ from .main import read_feature_table
 from .gencode import load_gencode_annotation
 from .refseq import load_refseq_annotation
 from .gff2bed import gff2bed
+from .intersect import bed_intersect
 
 __all__ = [
     'read_feature_table',
     'load_gencode_annotation',
     'load_refseq_annotation',
-    'gff2bed'
+    'gff2bed',
+    'bed_intersect'
 ]
