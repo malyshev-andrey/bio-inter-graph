72d4c730f7d4d57aa187819c30fcfc5d77993a3b
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2024-11-24 19:41:14 +0300

    Fix circular import bug

--- biointergraph/ids/__init__.py
+++ biointergraph/ids/__init__.py
@@ -1,7 +1,3 @@
 from .utils import is_valid_id, drop_id_version
-from .info import retrieve_gene_id4refseq_transcripts
 
-__all__ = [
-    'is_valid_id', 'drop_id_version',
-    'retrieve_gene_id4refseq_transcripts'
-]
+__all__ = ['is_valid_id', 'drop_id_version']
---
+++ biointergraph/ids_info/__init__.py
@@ -0,0 +1,4 @@
+from .main import retrieve_gene_id4refseq_transcripts
+
+
+__all__ = ['retrieve_gene_id4refseq_transcripts']
