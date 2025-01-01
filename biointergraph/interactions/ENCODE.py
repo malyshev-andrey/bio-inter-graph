a32490630be0eb863f9a89bc27516e8b336dfc5b
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2025-01-01 18:10:55 +0300

    Refine and add docstring for load_encode_metadata function

--- biointergraph/interactions/ENCODE.py
+++ biointergraph/interactions/ENCODE.py
@@ -3,7 +3,43 @@ from urllib.parse import urlencode
 import pandas as pd
 
 
-def _load_encode_metadata(*, cell_line: str|None = None, assay: str, **kwargs) -> pd.DataFrame:
+def load_encode_metadata(*, cell_line: str|None = None, assay: str, **kwargs) -> pd.DataFrame:
+    """
+    Loads metadata for ENCODE project files based on the specified cell line and assay.
+
+    This function queries the ENCODE database for metadata related to the specified assay (experimental protocol)
+    and optionally filters by a given cell line. It returns the result as a pandas DataFrame containing the metadata
+    for released files that match the query parameters.
+
+    Parameters:
+    -----------
+    cell_line : str or None, optional
+        The cell line to filter the metadata by. If None, no filtering by cell line is applied.
+
+    assay : str
+        The assay (experimental protocol) to filter the metadata by. This is a required parameter.
+
+    **kwargs : keyword arguments
+        Additional parameters to pass as filters for the ENCODE metadata query. These will be appended to the query
+        parameters in the URL (e.g., for additional filtering based on specific file types or statuses).
+
+    Returns:
+    --------
+    pd.DataFrame
+        A pandas DataFrame containing the metadata of files retrieved from the ENCODE project database, with columns
+        for file attributes such as file format, file size, and more. The dataframe will not contain fully empty columns.
+
+    Raises:
+    -------
+    AssertionError
+        If no files are found matching the query parameters, an assertion error is raised.
+
+    Example:
+    --------
+    >>> load_encode_metadata(assay="eCLIP", cell_line="K562")
+    This will return metadata for the eCLIP assays conducted on the K562 cell line.
+    """
+
     params = {
         'type': 'File',
         'assay_title': assay,
@@ -17,6 +53,8 @@ def _load_encode_metadata(*, cell_line: str|None = None, assay: str, **kwargs) -
     metadata = pd.read_csv(url, sep='\t', skiprows=1, dtype='str')
 
     metadata = metadata.loc[:, ~metadata.isna().all()]
+
+    assert metadata.shape[0] > 0, 'No files found!'
 
     return metadata
 
--- biointergraph/interactions/__init__.py
+++ biointergraph/interactions/__init__.py
@@ -1,11 +1,12 @@
 from .karr_seq import load_karr_seq_data, karr_seq_data2pairwise
 from .ric_seq import load_ric_seq_data
-from .ENCODE import load_encode_eCLIP
+from .ENCODE import load_encode_metadata, load_encode_eCLIP
 
 
 __all__ = [
     'load_karr_seq_data',
     'karr_seq_data2pairwise',
     'load_ric_seq_data',
+    'load_encode_metadata'
     'load_encode_eCLIP'
 ]
