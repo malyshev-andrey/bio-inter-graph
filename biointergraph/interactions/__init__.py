97438925c84bc25ab1ba9353c62895a101b145c9
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2025-01-02 03:00:39 +0300

    Add encode_eCLIP2pairwise function to __init__

--- biointergraph/interactions/__init__.py
+++ biointergraph/interactions/__init__.py
@@ -1,12 +1,11 @@
 from .karr_seq import load_karr_seq_data, karr_seq_data2pairwise
 from .ric_seq import load_ric_seq_data
-from .ENCODE import load_encode_metadata, load_encode_eCLIP
+from .ENCODE import load_encode_metadata, load_encode_eCLIP, encode_eCLIP2pairwise
 
 
 __all__ = [
     'load_karr_seq_data',
     'karr_seq_data2pairwise',
     'load_ric_seq_data',
-    'load_encode_metadata'
-    'load_encode_eCLIP'
+    'load_encode_metadata' 'load_encode_eCLIP', 'encode_eCLIP2pairwise'
 ]
