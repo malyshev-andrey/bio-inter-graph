703fda89c576476cc0010b9d670e54a81c62869b
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2025-01-04 01:05:20 +0300

    Refine karr_seq_ids2entrezgene_id function

--- biointergraph/ids_mapping/__init__.py
+++ biointergraph/ids_mapping/__init__.py
@@ -3,6 +3,7 @@ from .BioMart import load_BioMart_pairwise
 from .intersect import gencode_refseq_intersect2pairwise, extended_refseq_intersect2pairwise
 from .main import id2yagid
 from .entrez import retrieve_gene_id4refseq_transcripts
+from .karr_seq import karr_seq_ids2entrezgene_id
 
 
 __all__ = [
--- biointergraph/ids_mapping/karr_seq.py
+++ biointergraph/ids_mapping/karr_seq.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 import pandas as pd
 
 from ..shared import memory
@@ -8,9 +10,22 @@ from ..ids import drop_id_version
 @memory.cache
 def karr_seq_ids2entrezgene_id():
     ids = set()
-    for url in _retrieve_karr_seq_metadata()['url']:
-        data = _load_single_karr_seq(url, filter_func=lambda df: df[['seqid1', 'seqid2']])
-        ids.update(drop_id_version(data['seqid1']))
-        ids.update(drop_id_version(data['seqid2']))
 
-    return pd.Series(ids)
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        futures = []
+        for url in _retrieve_karr_seq_metadata()['url']:
+            futures.append(executor.submit(
+                _load_single_karr_seq,
+                url, filter_func=lambda df: df[['seqid1', 'seqid2']]
+            ))
+
+        for future in as_completed(futures):
+            result = future.result()
+            ids.update(result['seqid1'])
+            ids.update(result['seqid2'])
+
+
+    ids = pd.Series(list(ids))
+    ids = drop_id_version(ids)
+    assert ids.is_unique
+    return ids
