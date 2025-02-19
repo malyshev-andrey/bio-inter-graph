4a96b939372615092bb091701dd77c57dde63a28
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2025-02-19 03:19:28 +0300

    Add load_string_interactions and load_biogrid_interactions functions

--- biointergraph/interactions/__init__.py
+++ biointergraph/interactions/__init__.py
@@ -2,7 +2,7 @@ from .main import summarize_pairwise
 from .karr_seq import load_karr_seq_data, karr_seq_data2pairwise
 from .ric_seq import load_ric_seq_data
 from .ENCODE import load_encode_metadata, encode_eCLIP2pairwise
-from .protein import ensembl_protein2biogrid_id
+from .protein import ensembl_protein2biogrid_id, load_string_interactions, load_biogrid_interactions
 
 
 __all__ = [
@@ -11,5 +11,5 @@ __all__ = [
     'karr_seq_data2pairwise',
     'load_ric_seq_data',
     'load_encode_metadata', 'encode_eCLIP2pairwise',
-    'ensembl_protein2biogrid_id'
+    'ensembl_protein2biogrid_id', 'load_string_interactions', 'load_biogrid_interactions'
 ]
--- biointergraph/interactions/protein.py
+++ biointergraph/interactions/protein.py
@@ -8,7 +8,7 @@ from ..shared import CHUNKSIZE, memory
 @memory.cache
 def ensembl_protein2biogrid_id(
         chunksize: int | None = CHUNKSIZE
-    ) -> pd.DataFrame:
+    ) -> pd.Series:
     url = 'https://downloads.thebiogrid.org/Download/BioGRID/Latest-Release/BIOGRID-IDENTIFIERS-LATEST.tab.zip'
     filter_func = lambda df: df[
         df['ORGANISM_OFFICIAL_NAME'].eq('Homo sapiens') &
@@ -28,7 +28,52 @@ def ensembl_protein2biogrid_id(
         result = pd.concat(result)
 
     result = result[['BIOGRID_ID', 'IDENTIFIER_VALUE']]
+    result = result.rename(columns={
+        'BIOGRID_ID': 'biogrid_id',
+        'IDENTIFIER_VALUE': 'ensembl_protein'
+    })
     assert not result.duplicated().any()
-    result = result.set_index('IDENTIFIER_VALUE', verify_integrity=True)
-    result = result['BIOGRID_ID']
+    result = result.set_index('ensembl_protein', verify_integrity=True)
+    result = result['biogrid_id']
+    return result
+
+
+def load_biogrid_interactions() -> pd.DataFrame:
+    url = 'https://downloads.thebiogrid.org/Download/BioGRID/Latest-Release/BIOGRID-MV-Physical-LATEST.tab3.zip'
+    result = pd.read_csv(url, sep='\t', dtype='str')
+    result = result[
+        result['Organism ID Interactor A'].eq('9606') &
+        result['Organism ID Interactor B'].eq('9606') &
+        ~result['Experimental System'].isin(['Protein-RNA', 'Affinity Capture-RNA'])
+    ]
+    assert (
+        result['Organism Name Interactor A'].eq('Homo sapiens') &
+        result['Organism Name Interactor B'].eq('Homo sapiens')
+    ).all()
+
+    result = result[['BioGRID ID Interactor A', 'BioGRID ID Interactor B']]
+    result = result.rename(columns={
+        'BioGRID ID Interactor A': 'biogrid_id1',
+        'BioGRID ID Interactor B': 'biogrid_id2'
+    })
+    result = result.drop_duplicates()
+    return result
+
+
+def load_string_interactions() -> pd.DataFrame:
+    url = 'https://stringdb-downloads.org/download/stream/protein.physical.links.v12.0/9606.protein.physical.links.v12.0.onlyAB.tsv.gz'
+    result = pd.read_csv(url, sep='\t')
+    assert (
+        result['protein1'].str.match('^9606.ENSP\d{11}$') &
+        result['protein2'].str.match('^9606.ENSP\d{11}$')
+    ).all()
+    result['protein1'] = result['protein1'].str.removeprefix('9606.')
+    result['protein2'] = result['protein2'].str.removeprefix('9606.')
+
+    result = result[['protein1', 'protein2']]
+    result = result.rename(columns={
+        'protein1': 'ensembl_protein1',
+        'protein2': 'ensembl_protein2'
+    })
+    assert not result.duplicated().any()
     return result
