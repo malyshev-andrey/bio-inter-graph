5fea04e6ab3b8bfc020547255ecbde01ec2e94e9
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2025-03-09 16:27:18 +0300

    Add load_encode_ChIP_seq_peaks function

--- biointergraph/interactions/__init__.py
+++ biointergraph/interactions/__init__.py
@@ -4,6 +4,7 @@ from .ric_seq import load_ric_seq_data
 from .ENCODE import load_encode_metadata, encode_eCLIP2pairwise
 from .protein import load_string_interactions, load_biogrid_interactions, load_IntAct_interactions
 from .graph import build_main_graph, describe_graph, detect_communities, describe_nodes
+from .chip_seq import load_encode_ChIP_seq_peaks
 
 
 __all__ = [
@@ -14,5 +15,6 @@ __all__ = [
     'load_string_interactions',
     'load_biogrid_interactions',
     'load_IntAct_interactions',
-    'build_main_graph', 'describe_graph', 'detect_communities', 'describe_nodes'
+    'build_main_graph', 'describe_graph', 'detect_communities', 'describe_nodes',
+    'load_encode_ChIP_seq_peaks'
 ]
---
+++ biointergraph/interactions/chip_seq.py
@@ -0,0 +1,65 @@
+from urllib.parse import urlencode
+
+import pandas as pd
+from tqdm.auto import tqdm
+
+from ..annotations import bed_merge, sanitize_bed
+
+
+def _load_encode_ChIP_seq_metadata(cell_line: str|None = None) -> pd.DataFrame:
+
+    params = [
+        ('assay_title', 'TF ChIP-seq'),
+        ('status', 'released'),
+        ('file_format', 'bed'),
+        ('assembly', 'GRCh38'),
+        ('output_type', 'IDR thresholded peaks'),
+        ('output_type', 'conservative IDR thresholded peaks')
+    ]
+    if cell_line is not None:
+        params.append(('biosample_ontology.term_name', cell_line))
+    params = urlencode(params)
+
+    url = f'https://www.encodeproject.org/report.tsv?type=File&{params}'
+    metadata = pd.read_csv(url, sep='\t', skiprows=1, dtype='str')
+
+    n_experiments = metadata['Dataset'].nunique()
+
+    metadata['is_conservative'] = metadata['Output type'].eq('conservative IDR thresholded peaks')
+    metadata = metadata[
+        ~metadata.groupby(['Dataset'])['is_conservative'].transform('any')
+        | metadata['is_conservative']
+    ]
+
+    assert metadata['Dataset'].nunique() == n_experiments
+
+    metadata['Date created'] = pd.to_datetime(metadata['Date created'])
+    latest_file_date = metadata.groupby('Dataset')['Date created'].transform('max')
+    metadata = metadata[metadata['Date created'] == latest_file_date]
+    assert metadata['Dataset'].is_unique
+
+    return metadata
+
+
+def load_encode_ChIP_seq_peaks(cell_line: str|None = None) -> pd.DataFrame:
+    metadata = _load_encode_ChIP_seq_metadata(cell_line)
+
+    result = []
+    with tqdm(desc='Peaks:') as progress_bar:
+        for _, row in tqdm(metadata.iterrows(), total=metadata.shape[0], desc='Experiments:'):
+            bed = pd.read_csv(
+                f'https://www.encodeproject.org{row["Download URL"]}',
+                sep='\t', usecols=range(6),
+                header=None, names=BED_COLUMNS,
+                dtype='str'
+            )
+            bed['name'] = row['Target label']
+            result.append(bed)
+            progress_bar.update(bed.shape[0])
+    result = pd.concat(result)
+    result = sanitize_bed(result)
+
+    result = bed_merge(result, by='Name')
+    result['score'], result['strand'] = 1000, '.'
+
+    return result
