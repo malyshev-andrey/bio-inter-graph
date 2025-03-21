6fbf2cdcf21e7ab481e4ebbe151151abcb7b44d5
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2025-03-21 18:37:20 +0300

    Add _read_tsv function to shared

--- biointergraph/shared.py
+++ biointergraph/shared.py
@@ -1,5 +1,10 @@
 import os
+from pathlib import Path
+from typing import Callable, IO
+
 from joblib import Memory
+import pandas as pd
+from tqdm.auto import tqdm
 
 # schemas
 GFF_COLUMNS = [
@@ -27,7 +32,7 @@ UNIFY_BIOTYPES = {
 
 GOOGLE_DRIVE_URL = 'https://drive.usercontent.google.com/download?id={id}&export=download&confirm=t'
 
-CHUNKSIZE = 10000
+CHUNKSIZE = int(1e4)
 
 
 # cache config
@@ -37,3 +42,31 @@ cache_dir = os.path.join(
 )
 os.makedirs(cache_dir, exist_ok=True)
 memory = Memory(cache_dir, verbose=0)
+
+
+def _read_tsv(
+        filepath_or_buffer: str | Path | IO[str], *,
+        filter_func: Callable[[pd.DataFrame], pd.DataFrame] = lambda df: df,
+        chunksize: int | None = CHUNKSIZE,
+        desc: str | None = None,
+        **kwargs
+    ) -> pd.DataFrame:
+
+    read_csv_kwargs = dict(
+        sep='\t',
+        dtype='str'
+    )
+    read_csv_kwargs.update(kwargs)
+
+    if chunksize is None:
+        return filter_func(pd.read_csv(filepath_or_buffer, **read_csv_kwargs))
+
+    if desc is None:
+        desc = filepath_or_buffer if isinstance(filepath_or_buffer, str) else ''
+    with tqdm(desc=desc, unit='row') as progress_bar:
+        result = []
+        for chunk in pd.read_csv(filepath_or_buffer, chunksize=chunksize, **read_csv_kwargs):
+            progress_bar.update(chunk.shape[0])
+            result.append(filter_func(chunk))
+        result = pd.concat(result)
+    return result
