17e7017b149ada260b09068445d40df7c4d02c25
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2025-03-22 22:09:49 +0300

    Refine load_IntAct_interactions function

--- biointergraph/interactions/protein.py
+++ biointergraph/interactions/protein.py
@@ -1,10 +1,4 @@
-import tempfile
-import zipfile
-from typing import Callable
-
-import requests
 import pandas as pd
-from tqdm.auto import tqdm
 
 from ..shared import memory, _read_tsv
 from ..ids_mapping import id2yapid
@@ -80,83 +74,54 @@ def load_string_interactions(min_score: int = 700) -> pd.DataFrame:
 
 
 @memory.cache
-def load_IntAct_interactions(
-        filter_func: Callable[[pd.DataFrame], pd.DataFrame]|None = None
-    ) -> pd.DataFrame:
-    if filter_func is None:
-        filter_func = lambda df: df[
+def load_IntAct_interactions() -> pd.DataFrame:
+    result = _read_tsv(
+        'https://ftp.ebi.ac.uk/pub/databases/intact/current/psimitab/species/human.txt',
+        usecols=[
+            '#ID(s) interactor A',
+            'ID(s) interactor B',
+            'Interaction detection method(s)',
+            'Publication Identifier(s)',
+            'Taxid interactor A',
+            'Taxid interactor B',
+            'Interaction type(s)',
+            'Type(s) interactor A',
+            'Type(s) interactor B'
+        ],
+        filter_func=lambda df: df[
             df['Type(s) interactor A'].eq('psi-mi:"MI:0326"(protein)') &
             df['Type(s) interactor B'].eq('psi-mi:"MI:0326"(protein)') &
-            df['ID(s) interactor A'].str.startswith('uniprotkb:') &
-            df['ID(s) interactor B'].str.startswith('uniprotkb:') &
             df['Taxid interactor A'].eq('taxid:9606(human)|taxid:9606(Homo sapiens)') &
             df['Taxid interactor B'].eq('taxid:9606(human)|taxid:9606(Homo sapiens)') &
-            df['Interaction type(s)'].isin([
+            df['Interaction type(s)'].isin({
                 'psi-mi:"MI:0914"(association)',
                 'psi-mi:"MI:0915"(physical association)',
                 'psi-mi:"MI:0407"(direct interaction)'
-            ]) &
-            (df['ID(s) interactor A'] != df['ID(s) interactor B'])
-        ]
-
-    url = 'https://ftp.ebi.ac.uk/pub/databases/intact/current/psimitab/species/human.zip'
-    with tempfile.NamedTemporaryFile(suffix=".zip", delete=True) as db_archive:
-        response = requests.get(url, stream=True)
-        response.raise_for_status()
-
-        total_size = int(response.headers.get("content-length", 0))
-        with tqdm(desc=url, total=total_size, unit="B", unit_scale=True) as progress_bar:
-            for chunk in response.iter_content(chunk_size=8192):
-                progress_bar.update(len(chunk))
-                db_archive.write(chunk)
-        db_archive.flush()
-
-        with zipfile.ZipFile(db_archive.name, 'r') as z:
-            with z.open('human.txt') as db:
-                result = pd.read_csv(db, sep='\t')
-    result = result.rename(columns={'#ID(s) interactor A': 'ID(s) interactor A'})
-    result = filter_func(result)
-
-    uniprot_id_regex = r'^uniprotkb:(.{6}(-(\d+|PRO_\d{10}))?|.{10})$'
-    assert (
-        result['ID(s) interactor A'].str.match(uniprot_id_regex).all() and
-        result['ID(s) interactor B'].str.match(uniprot_id_regex).all()
+            })
+        ],
+        na_values=['-']
     )
-    result['ID(s) interactor A'] = result['ID(s) interactor A'].str.removeprefix('uniprotkb:')
-    result['ID(s) interactor B'] = result['ID(s) interactor B'].str.removeprefix('uniprotkb:')
 
-    result = result[['ID(s) interactor A', 'ID(s) interactor B', 'Publication Identifier(s)']]
-
-    n = result.shape[0]
-    ids = ['ID(s) interactor A', 'ID(s) interactor B']
-    swap = dict(zip(ids, ids[::-1]))
-    result = pd.concat([
-        result,
-        result.rename(columns=swap)
-    ])
-    result = result[result['ID(s) interactor A'] < result['ID(s) interactor B']]
-    assert result.shape[0] == n
-
-    result['PMID'] = result['Publication Identifier(s)'].str.extract('pubmed:(\d+)')
+    result['PMID'] = result['Publication Identifier(s)'].str.extract(r'pubmed:(\d+)', expand=False)
     result = result[~result['PMID'].isna()]
-    result['n_publications'] = result.groupby(ids)['PMID'].transform('nunique')
+    assert not result['Interaction detection method(s)'].isna().any()
 
-    result = result[result['n_publications'] > 1]
-
-    result['yapid1'] = id2yapid(result['ID(s) interactor A'])
-    result['yapid2'] = id2yapid(result['ID(s) interactor B'])
+    result['yapid1'] = id2yapid(data['#ID(s) interactor A'].str.removeprefix('uniprotkb:'))
+    result['yapid2'] = id2yapid(data['ID(s) interactor B'].str.removeprefix('uniprotkb:'))
     result = result[
         result['yapid1'].str.startswith('YAPID') &
         result['yapid2'].str.startswith('YAPID')
     ]
 
-    ids = ['yapid1', 'yapid2']
-    result = result[ids]
-    swap = dict(zip(ids, ids[::-1]))
     result = pd.concat([
         result,
-        result.rename(columns=swap)
+        result.rename(columns={'yapid1': 'yapid2', 'yapid2': 'yapid1'})
     ])
     result = result[result['yapid1'] < result['yapid2']]
-    result = result.drop_duplicates()
+
+    result = result.drop_duplicates(['yapid1', 'yapid2', 'PMID', 'Interaction detection method(s)'])
+    result = result.groupby(['yapid1', 'yapid2'], as_index=False).size()
+    result = result[result['size'] > 1]
+    result = result[['yapid1', 'yapid2']]
+
     return result
