7112c3db2a733566509bbc656aa2efae1902516b
Андрей Малышев <andrey.malyshev@MacBook-Pro-Andrej.local>
2025-03-23 13:32:36 +0300

    Refine load_postar3_data function

--- biointergraph/interactions/rna_protein.py
+++ biointergraph/interactions/rna_protein.py
@@ -3,6 +3,7 @@ import pandas as pd
 from ..annotations import load_gencode_bed, load_refseq_bed, bed_intersect
 from ..ids_mapping import id2yapid, id2yagid
 from ..shared import _read_tsv, memory, BED_COLUMNS
+from .main import _annotate_peaks
 
 
 def _load_postar3_peaks(species: str = 'human', **kwargs) -> pd.DataFrame:
@@ -29,40 +30,21 @@ def load_postar3_data(species: str, cell_line: str, annotation: str, **kwargs) -
         filter_func=lambda df: df[df['cell_line'].eq(cell_line)],
         **kwargs
     )
-    annotation_bed = {
+    annotation = {
         'gencode': load_gencode_bed,
         'refseq': load_refseq_bed
     }[annotation](assembly='hg38', feature='gene')
 
-    result = bed_intersect(
-        result[BED_COLUMNS],
-        annotation_bed,
-        unify_chr_assembly='hg38',
-        strandedness='same',
-        jaccard=True,
-        how='left'
+    result = _annotate_peaks(
+        result[BED_COLUMNS].copy(), annotation,
+        assembly='hg38',
+        desc='POSTAR3',
+        convert_ids=True
     )
-
-    no_intersect = result['start2'].eq(-1)
-    print(f'POSTAR3 peaks without intersections: {no_intersect.sum()}')
-    result = result[~no_intersect]
-
-    peak_id = {f'{c}1': c for c in BED_COLUMNS}
-    result = result.rename(columns=peak_id)
-    result = result.sort_values('jaccard')
-    result = result.drop_duplicates(peak_id.values(), keep='last')
-
-    result['yagid'] = id2yagid(result['name2'])
-    assert result['yagid'].str.startswith('YAGID').all()
-    result['yapid'] = id2yapid('SYMBOL:' + result['name'])
-    assert result['yapid'].str.startswith('YAPID').all()
-
-    result = result[['yagid', 'yapid']]
-    result = result.drop_duplicates()
 
     return result
 
-
+c
 def load_frip_seq_data() -> pd.DataFrame:
     result = pd.concat([
         pd.read_csv(
