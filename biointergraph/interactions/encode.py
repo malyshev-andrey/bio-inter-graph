a5e2c18dcc06a410ff4e7b1fb8c0420eb5e4ec8c
Andrey Malyshev <malyshev.andrey.d@gmail.com>
2025-12-08 01:44:45 +0300

    Add ENCODE TF ChIP-seq interactions weights

--- biointergraph/interactions/encode.py
+++ biointergraph/interactions/encode.py
@@ -8,12 +8,19 @@ from tqdm.auto import tqdm
 from ..shared import memory, BED_COLUMNS
 from ..annotations import (
     load_refseq_bed, load_gencode_bed,
-    sanitize_bed, bed_merge,
+    sanitize_bed, bed_cluster,
     load_chromhmm_annotation
 )
 from ..ids_mapping import id2yapid
 from .main import _annotate_peaks
 
+
+IDR_BED_COLUMNS = BED_COLUMNS + [
+    'signalValue',
+    'p_value', 'q_value',
+    'summit',
+    'localIDR', 'globalIDR'
+]
 
 def load_encode_metadata(
         assay: str|Iterable[str] = (), *,
@@ -62,12 +69,13 @@ def load_encode_metadata(
 
 def _encode_metadata_row2bed(
         row: pd.Series,
-        features: str|dict|Iterable[str]|None = None
+        features: str|dict|Iterable[str]|None = None,
+        colnames: list[str] = BED_COLUMNS
     ) -> pd.DataFrame:
     bed = pd.read_csv(
         f'https://www.encodeproject.org{row["Download URL"]}',
-        sep='\t', usecols=range(6),
-        header=None, names=BED_COLUMNS,
+        sep='\t', usecols=range(len(colnames)),
+        header=None, names=colnames,
         dtype='str'
     )
     bed['name'] = row['Target label']
@@ -86,7 +94,8 @@ def _encode_metadata2bed(
         files: pd.DataFrame, *,
         features: str|dict|Iterable[str]|None = None,
         desc: str|None = None,
-        stranded: bool = True
+        stranded: bool = True,
+        colnames: list[str] = BED_COLUMNS
     ) -> pd.DataFrame:
     if desc is None:
         assay = files['Assay term name'].unique().item()
@@ -99,7 +108,7 @@ def _encode_metadata2bed(
         for _, row in files.iterrows():
             futures.append(executor.submit(
                 _encode_metadata_row2bed,
-                row, features
+                row, features, colnames
             ))
 
         tqdm_kwargs = dict(desc=desc, total=len(futures), unit='file')
@@ -220,21 +229,11 @@ def _load_encode_chip_seq_bed(assembly: str, cell_line: str|None = None) -> pd.D
         assembly=assembly
     )
 
-    output_types = {'IDR thresholded peaks', 'conservative IDR thresholded peaks'}
-    metadata = metadata[metadata['Output type'].isin(output_types)]
-
-    n_experiments = metadata['Dataset'].nunique()
-
-    metadata['is_conservative'] = metadata['Output type'].eq('conservative IDR thresholded peaks')
     metadata = metadata[
-        ~metadata.groupby(['Dataset'])['is_conservative'].transform('any')
-        | metadata['is_conservative']
+        metadata['Output type'].eq('IDR ranked peaks') &
+        metadata['Analysis step version'].eq('/analysis-step-versions/tf-chip-seq-replicated-idr-step-v-1-0/')
     ]
-
-    assert metadata['Dataset'].nunique() == n_experiments
-
-    metadata['Date created'] = pd.to_datetime(metadata['Date created'])
-    metadata = metadata.sort_values('Date created')
+    metadata = metadata.sort_values('File size')
     metadata = metadata.drop_duplicates('Dataset', keep='last')
 
     metadata = metadata[
@@ -242,9 +241,16 @@ def _load_encode_chip_seq_bed(assembly: str, cell_line: str|None = None) -> pd.D
         ~metadata['Target label'].isin({'POLR2AphosphoS5', 'POLR2AphosphoS2'})
     ]
 
-    result = _encode_metadata2bed(metadata, stranded=False)
+    result = _encode_metadata2bed(metadata, stranded=False, colnames=IDR_BED_COLUMNS)
 
-    result = bed_merge(result, by='Name', score_agg_func='max')
+    result = bed_cluster(result, by='Name')
+
+    result['globalIDR'] = result['globalIDR'].astype('float')
+    result = result.groupby(['chr', 'name', 'Cluster'], as_index=False).agg(
+        start=('start', 'min'),
+        end=('end', 'max'),
+        weight=('globalIDR', 'max')
+    )
 
     return result
 
